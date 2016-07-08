b4b16e99ec2eef375ae40faff638a4bb0d5c310b
saposki <saposki@gmail.com>
2016-07-08 01:17:59 -0700

    added integration enviroment

---
+++ saposki/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'saposki',
+        'USER': 'saposkiuser',
+        'PASSWORD': 'sappskipassword',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
--- saposki/settings.py
+++ saposki/settings.py
@@ -81,8 +81,14 @@ WSGI_APPLICATION = 'saposki.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'saposki',
+        'USER': 'saposkiuser',
+        'PASSWORD': 'sappskipassword',
+        'HOST': 'localhost',
+        'PORT': '',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -137,10 +143,30 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 
 # import dj_database_url
-# DATABASES['default'] = dj_database_url.config()
+# # DATABASES = { 'default' : dj_database_url.config()}
 #
 # # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 #
 # # Allow allhost headers
 # ALLOWED_HOSTS  = ['*']
+#
+# # try to load local_settings.py if it exists
+# try:
+#   from local_settings import *
+# except Exception as e:
+#   pass
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#     }
+# }
+
+# if not DEBUG:
+#    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+#    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+#    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+#    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+#    STATIC_URL = S3_URL
