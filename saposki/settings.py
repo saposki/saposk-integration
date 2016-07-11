a92a40d5e00a32b4a24c467f30aa7964d50d1a86
saposki <saposki@gmail.com>
2016-07-10 21:41:55 -0700

    modified settings

--- saposki/settings.py
+++ saposki/settings.py
@@ -84,18 +84,18 @@ WSGI_APPLICATION = 'saposki.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         # 'NAME': 'saposki',
-#         # 'USER': 'saposkiuser',
-#         # 'PASSWORD': 'sappskipassword',
-#         # 'HOST': 'localhost',
-#         # 'PORT': '',
-#         # 'ENGINE': 'django.db.backends.sqlite3',
-#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'saposki',
+        # 'USER': 'saposkiuser',
+        # 'PASSWORD': 'sappskipassword',
+        # 'HOST': 'localhost',
+        # 'PORT': '',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
@@ -172,7 +172,6 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 
-import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
 # DATABASES = { 'default' : dj_database_url.config()}
